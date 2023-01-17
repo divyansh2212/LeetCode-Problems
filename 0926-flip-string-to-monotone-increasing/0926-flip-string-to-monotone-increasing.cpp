568318a98529f46d57dd887384f01d3f73ccfb23
Divyansh Mittal <90034292+divyansh2212@users.noreply.github.com>
2023-01-17 12:03:00 +0530

    Time: 275 ms (11.14%), Space: 53.1 MB (12.40%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -48,11 +48,12 @@ class Solution {
 public:
     int minFlipsMonoIncr(string s) {
         int n = s.length();
-        vector<vector<int>> dp(n + 1, vector<int> (2, 0));
-        // return minmCost(0, 0, s, dp);
+        // vector<vector<int>> dp(n + 1, vector<int> (2, 0));
+        vector<int> next(2, 0);
         
         for(int idx = n - 1; idx >= 0; idx--)
         {
+            vector<int> curr(2, 0);
             for(int last = 0; last <= 1; last++)
             {
                 if(last == 0)
@@ -61,37 +62,38 @@ public:
                     if(s[idx] == '1')
                     {
                         // making it 0
-                        way1 = 1 + dp[idx + 1][0];
+                        way1 = 1 + next[0];
                 
                         // keeping it 1
-                        way2 = dp[idx + 1][1];
+                        way2 = next[1];
                     }
             
                     else
                     {
                         // keeping it 0
-                        way1 = dp[idx + 1][0];
+                        way1 = next[0];
                 
                         // making it 1
-                        way2 = 1 + dp[idx + 1][1];
+                        way2 = 1 + next[1];
                     }
             
-                    dp[idx][last] = min(way1, way2);
+                    curr[last] = min(way1, way2);
                 }
         
                 else
                 {
                     int way1;
                     if(s[idx] == '1')
-                        way1 = dp[idx + 1][1];
+                        way1 = next[1];
                     else
-                        way1 = 1 + dp[idx + 1][1];
+                        way1 = 1 + next[1];
             
-                    dp[idx][last] = way1;
+                    curr[last] = way1;
                 }
             }
+            next = curr;
         }
         
-        return min(dp[0][0], dp[0][1]);
+        return min(next[0], next[1]);
     }
 };
