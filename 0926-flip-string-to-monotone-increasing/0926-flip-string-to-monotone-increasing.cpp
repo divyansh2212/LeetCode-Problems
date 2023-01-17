73def6c4bcbf71d706f2e35b562f77ffd4d4c96a
Divyansh Mittal <90034292+divyansh2212@users.noreply.github.com>
2023-01-17 12:00:38 +0530

    Time: 281 ms (10.38%), Space: 78.9 MB (6.83%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -47,7 +47,51 @@ class Solution {
     
 public:
     int minFlipsMonoIncr(string s) {
-        vector<vector<int>> dp(s.length(), vector<int> (2, -1));
-        return minmCost(0, 0, s, dp);
+        int n = s.length();
+        vector<vector<int>> dp(n + 1, vector<int> (2, 0));
+        // return minmCost(0, 0, s, dp);
+        
+        for(int idx = n - 1; idx >= 0; idx--)
+        {
+            for(int last = 0; last <= 1; last++)
+            {
+                if(last == 0)
+                {
+                    int way1 = 1e9, way2 = 1e9;
+                    if(s[idx] == '1')
+                    {
+                        // making it 0
+                        way1 = 1 + dp[idx + 1][0];
+                
+                        // keeping it 1
+                        way2 = dp[idx + 1][1];
+                    }
+            
+                    else
+                    {
+                        // keeping it 0
+                        way1 = dp[idx + 1][0];
+                
+                        // making it 1
+                        way2 = 1 + dp[idx + 1][1];
+                    }
+            
+                    dp[idx][last] = min(way1, way2);
+                }
+        
+                else
+                {
+                    int way1;
+                    if(s[idx] == '1')
+                        way1 = dp[idx + 1][1];
+                    else
+                        way1 = 1 + dp[idx + 1][1];
+            
+                    dp[idx][last] = way1;
+                }
+            }
+        }
+        
+        return min(dp[0][0], dp[0][1]);
     }
 };
