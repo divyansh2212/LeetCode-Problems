b47be4db7dd849aedc9b748a3ce558c930ade86e
Divyansh Mittal <90034292+divyansh2212@users.noreply.github.com>
2023-02-12 11:58:32 +0530

    Time: 304 ms (50.00%), Space: 53.5 MB (16.67%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -1,51 +1,59 @@
-// <--------------------Jai Shree Babosa-------------------->
+    // <--------------------Jai Shree Babosa-------------------->
 
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define inf INT_MAX
-const int mod = 1e9 + 7;
+    #include <bits/stdc++.h>
+    using namespace std;
+    #define ll long long
+    #define inf INT_MAX
+    const int mod = 1e9 + 7;
 
-class Solution
-{
-public:
-    long long countFairPairs(vector<int> &nums, int lower, int upper)
+    class Solution
     {
-        long long ans = 0;
-        sort(nums.begin(), nums.end());
 
-        int n = nums.size();
-
-        for (int i = 0; i < n - 1; i++)
+        void lowerbound(int lo, int hi, vector<int> &nums, int i, int &lower, int &upper, int &idx)
         {
-            int idx1 = -1, idx2 = -1, lo = i + 1, hi = n - 1;
-
             while (lo <= hi)
             {
                 int mid = lo + (hi - lo) / 2;
 
                 if (nums[mid] + nums[i] >= lower)
-                    hi = mid - 1, idx1 = mid;
+                    hi = mid - 1, idx = mid;
                 else
                     lo = mid + 1;
             }
-
-            lo = i + 1, hi = n - 1;
+        }
+        void upperbound(int lo, int hi, vector<int> &nums, int i, int &lower, int &upper, int &idx)
+        {
             while (lo <= hi)
             {
                 int mid = lo + (hi - lo) / 2;
 
                 if (nums[mid] + nums[i] <= upper)
-                    lo = mid + 1, idx2 = mid;
+                    lo = mid + 1, idx = mid;
                 else
                     hi = mid - 1;
             }
-
-            int len = idx2 - idx1 + 1;
-
-            if (len > 0 && idx1 != -1 && idx2 != -1)
-                ans += len;
         }
-        return ans;
-    }
-};
+
+    public:
+        long long countFairPairs(vector<int> &nums, int lower, int upper)
+        {
+            long long ans = 0;
+            sort(nums.begin(), nums.end());
+
+            int n = nums.size();
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int idx1 = -1, idx2 = -1;
+
+                lowerbound(i + 1, n - 1, nums, i, lower, upper, idx1);
+                upperbound(i + 1, n - 1, nums, i, lower, upper, idx2);
+
+                int len = idx2 - idx1 + 1;
+
+                if (len > 0 && idx1 != -1 && idx2 != -1)
+                    ans += len;
+            }
+            return ans;
+        }
+    };
