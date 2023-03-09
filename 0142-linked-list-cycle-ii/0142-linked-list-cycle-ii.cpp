3d99a8df8c29454d56d0c56d2884574673679257
Divyansh Mittal <90034292+divyansh2212@users.noreply.github.com>
2023-03-09 05:32:11 +0530

    Time: 8 ms (76.50%), Space: 7.6 MB (84.91%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,45 @@
+// Jai Shree Babosa!
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define inf INT_MAX
+const int mod = 1e9 + 7;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *detectCycle(ListNode *head)
+    {
+        if (head == NULL)
+            return NULL;
+
+        ListNode *fast = head, *slow = head;
+
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (fast == slow)
+            {
+                fast = head;
+                while (slow != fast)
+                {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
+    }
+};
