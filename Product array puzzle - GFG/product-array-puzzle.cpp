052712a6d0125e703c5f19e7f8f8460bde6db8ad
Divyansh Mittal <90034292+divyansh2212@users.noreply.github.com>
2023-04-16 23:49:03 +0530

    Added solution - LeetHub

---
+++ Product array puzzle - GFG/product-array-puzzle.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    // nums: given vector
+    // return the Product vector P that hold product except self at each index
+    vector<long long int> productExceptSelf(vector<long long int>& nums, int n) {
+       
+        //code here
+        vector<long long> ans(n, 0);
+        int zeros = 0;
+        for(int i = 0; i < n; i++)
+            if(nums[i] == 0)
+                zeros++;
+        if(zeros > 1)
+            return ans;
+        else if(zeros)
+        {
+            long long product = 1;
+            for(int i = 0; i < n; i++)
+            {
+                if(nums[i] != 0)
+                    product *= nums[i];
+            }
+            for(int i = 0; i < n; i++)
+                if(nums[i] == 0)
+                    ans[i] = product;
+            return ans;
+        }
+        else
+        {
+            long long product = 1;
+            for(int i = 0; i < n; i++)
+                product *= nums[i];
+            for(int i = 0; i < n; i++)
+                ans[i] = product / nums[i];
+            return ans;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+ {
+    int t;  // number of test cases
+    cin>>t;
+    while(t--)
+    {
+        int n;  // size of the array
+        cin>>n;
+        vector<long long int> arr(n),vec(n);
+        
+        for(int i=0;i<n;i++)    // input the array
+        {
+            cin>>arr[i];
+        }
+        Solution obj;
+        vec = obj.productExceptSelf(arr,n);   // function call
+        
+        for(int i=0;i<n;i++)    // print the output
+        {
+            cout << vec[i] << " ";
+        }
+        cout<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
