7f902ce82badd6465fcbc78626a82af8673173c6
Divyansh Mittal <90034292+divyansh2212@users.noreply.github.com>
2023-04-24 11:04:14 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (84.27%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq(stones.begin(), stones.end());
+        
+        while(pq.size() > 1)
+        {
+            int top1 = pq.top(); pq.pop();
+            int top2 = pq.top(); pq.pop();
+            if(top1 != top2)
+                pq.push(top1 - top2);
+        }
+        
+        if(pq.size() == 0)
+            return 0;
+        return pq.top();
+    }
+};
