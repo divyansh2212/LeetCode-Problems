ff3e981bb4334d3b69d771efed73786f800d2c0a
Divyansh Mittal <90034292+divyansh2212@users.noreply.github.com>
2023-07-04 21:59:50 +0530

    Time: 11 ms (46.18%), Space: 9.4 MB (72.62%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    int singleNumber(vector<int>& s) 
+    {
+    	vector<int> t(32);
+    	int sz = s.size();
+    	int i, j, n;
+    	for (i = 0; i < sz; ++i)
+    	{
+    		n = s[i];
+    		for (j = 31; j >= 0; --j)
+    		{
+    			t[j] += n & 1;
+    			n >>= 1;
+    			if (!n)
+    				break;
+    	    }
+        }
+	int res = 0;
+	for (j = 31; j >= 0; --j)
+	{
+		n = t[j] % 3;
+		if (n)
+			res += 1 << (31 - j);
+	}
+	return res;
+}
+
+    
+};
