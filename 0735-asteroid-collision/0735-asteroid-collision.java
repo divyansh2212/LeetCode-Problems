224aa00a4abbebc81651bfdfe8b74c61044041f9
Divyansh Mittal <90034292+divyansh2212@users.noreply.github.com>
2023-07-08 23:06:03 +0530

    Time: 16 ms (10.54%), Space: 44.1 MB (60.53%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,38 +1,55 @@
-import java.util.Stack;
-
 class Solution {
     public int[] asteroidCollision(int[] arr) {
+        
         int n = arr.length;
         Stack<Integer> st = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-            if (st.isEmpty()) {
+        
+        for(int i = 0; i < n; i++) 
+        {
+            if(st.size() == 0) {
                 st.push(arr[i]);
-            } 
-            else if (st.peek() > 0 && arr[i] > 0) {
+            }
+            else if(st.peek() > 0 && arr[i] > 0) {
                 st.push(arr[i]);
-            } 
-            else if (st.peek() > 0 && arr[i] < 0) {
-                while (!st.isEmpty() && st.peek() > 0 && st.peek() < Math.abs(arr[i])) {
-                    st.pop();
+            }
+            else if(st.peek() < 0 && arr[i] < 0) {
+                st.push(arr[i]);
+            }
+            
+            else if(st.peek() > 0 && arr[i] < 0) 
+            {
+                while(st.size() != 0) {
+                    if(st.peek() > Math.abs(arr[i]))
+                        break;
+                    else if(st.peek() == Math.abs(arr[i])) {
+                        st.pop();
+                        break;
+                    }
+                    else if(Math.abs(arr[i]) > st.peek()) {
+                        st.pop();
+                    }
+                    
+                    if(st.size() == 0 || st.peek() < 0) {
+                        st.push(arr[i]);
+                        break;
+                    }
                 }
-
-                if (!st.isEmpty() && st.peek() == Math.abs(arr[i])) {
-                    st.pop();
-                } else if (st.isEmpty() || st.peek() < 0) {
-                    st.push(arr[i]);
-                }
-            } 
-            else {
+            }
+            
+            else if(st.peek() < 0 && arr[i] > 0) 
+            {
                 st.push(arr[i]);
             }
         }
-
-        int[] ans = new int[st.size()];
-        for (int i = ans.length - 1; i >= 0; i--) {
-            ans[i] = st.pop();
+        
+        n = st.size();
+        int [] ans = new int[n];
+        int k = 0;
+        for(int x : st) {
+            ans[k] = x;
+            k++;
         }
-
+        
         return ans;
     }
 }
