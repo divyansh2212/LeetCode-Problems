6609dab5275b860d9da60d26ee6171f123f3568f
Divyansh Mittal <90034292+divyansh2212@users.noreply.github.com>
2023-07-09 00:34:12 +0530

    Time: 384 ms (24.40%), Space: 69.5 MB (16.15%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    long long putMarbles(vector<int>& weights, int k) {
-        int n = weights.size();
-        if (k == 1 || n == k) return 0;
-        vector<int> candidates;
-        for (int i = 0; i < n-1; i++)
-        {
-            candidates.push_back(weights[i] + weights[i+1]);
+    long long putMarbles(vector<int>& arr, int k) {
+        int n = arr.size();
+        
+        priority_queue<int> pq;
+        for(int i = 0; i < n - 1; i++) {
+            pq.push(arr[i] + arr[i + 1]);
         }
-        sort(candidates.begin(), candidates.end());
-        long long mins = 0, maxs = 0;
-        for (int i = 0; i < k-1; i++)
-        {
-            mins += candidates[i];
-            maxs += candidates[n-2-i];
+        
+        long maxm = 0;
+        for(int i = 0; i < k - 1; i++) {
+            maxm += pq.top();
+            pq.pop();
         }
-        return maxs - mins;
+        
+        priority_queue<int, vector<int>, greater<int>> pq2;
+        for(int i = 0; i < n - 1; i++) {
+            pq2.push(arr[i] + arr[i + 1]);
+        }
+        
+        long minm = 0;
+        for(int i = 0; i < k - 1; i++) {
+            minm += pq2.top();
+            pq2.pop();
+        }
+        
+        return maxm - minm;
     }
 };
