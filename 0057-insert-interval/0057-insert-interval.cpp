e2b08bda0a01907f8134d578070eb4c3b6577e17
Divyansh Mittal <90034292+divyansh2212@users.noreply.github.com>
2023-07-19 10:04:28 +0530

    Time: 23 ms (38.94%), Space: 17 MB (82.00%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,27 +1,32 @@
 class Solution {
+    
+    // bool static comparator(vector<int> &a, vector<int> &b)
+    // {
+    //     return a[0] < b[0];
+    // }
+    
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         
         intervals.push_back(newInterval);
-        
+        int n = intervals.size();
         sort(intervals.begin(), intervals.end());
         
-        int n = intervals.size();
-        
         vector<vector<int>> ans;
+        int start = intervals[0][0];
+        int end = intervals[0][1];
         
-        int prevEnd = intervals[0][1], prevStart = intervals[0][0];
-        for(int i = 1; i < n; i++)
-        {
-            if(prevEnd < intervals[i][0])
-            {
-                ans.push_back({prevStart, prevEnd});
-                prevStart = intervals[i][0], prevEnd = intervals[i][1];
+        for(int i = 0; i < n; i++) {
+            if((end >= intervals[i][0] && end <= intervals[i][1]) || (intervals[i][0] >= start && intervals[i][0] <= end)) {
+                start = min(intervals[i][0], start);
+                end = max(end, intervals[i][1]); 
             }
-            else
-                prevEnd = max(prevEnd, intervals[i][1]);
+            else {
+                ans.push_back({start, end});
+                start = intervals[i][0], end = intervals[i][1];
+            }
         }
-        ans.push_back({prevStart, prevEnd});
+        ans.push_back({start, end});
         
         return ans;
     }
