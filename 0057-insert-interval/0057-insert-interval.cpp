c71cd03b069c944997610abf57c53fb852b84179
Divyansh Mittal <90034292+divyansh2212@users.noreply.github.com>
2023-07-19 10:09:57 +0530

    Time: 7 ms (99.49%), Space: 16.9 MB (82.00%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -8,25 +8,22 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         
-        intervals.push_back(newInterval);
         int n = intervals.size();
-        sort(intervals.begin(), intervals.end());
         
         vector<vector<int>> ans;
-        int start = intervals[0][0];
-        int end = intervals[0][1];
+        int x = newInterval[0], y = newInterval[1];
         
         for(int i = 0; i < n; i++) {
-            if((end >= intervals[i][0] && end <= intervals[i][1]) || (intervals[i][0] >= start && intervals[i][0] <= end)) {
-                start = min(intervals[i][0], start);
-                end = max(end, intervals[i][1]); 
+            if((x >= intervals[i][0] && intervals[i][1] >= x) || (intervals[i][0] >= x && y >= intervals[i][0])) {
+                x = min(x, intervals[i][0]);
+                y = max(y, intervals[i][1]);
             }
             else {
-                ans.push_back({start, end});
-                start = intervals[i][0], end = intervals[i][1];
+                ans.push_back(intervals[i]);
             }
         }
-        ans.push_back({start, end});
+        ans.push_back({x, y});
+        sort(ans.begin(), ans.end());
         
         return ans;
     }
