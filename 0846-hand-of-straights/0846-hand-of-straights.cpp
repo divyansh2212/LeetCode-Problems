d68968c3cf72938b2877a50a4bde9c6c091a50da
Divyansh Mittal <90034292+divyansh2212@users.noreply.github.com>
2023-08-12 16:48:22 +0530

    Time: 65 ms (84.08%), Space: 29.6 MB (11.72%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+        sort(hand.begin(), hand.end());
+        
+        multiset<int> st;
+        for(int i = 0; i < n; i++) {
+            st.insert(hand[i]);
+        }
+        
+        for(int i = 0; i < n; i++) 
+        {
+            if(st.find(hand[i]) != st.end())
+            {
+                for(int j = hand[i]; j < hand[i] + groupSize; j++)
+                {
+                    if(st.find(j) != st.end()) {
+                        auto it = st.find(j);
+                        st.erase(it);
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return st.size() == 0;
+    }
+};
