80627af13b697a8af61094004f6c1bdf2af143cf
Divyansh Mittal <90034292+divyansh2212@users.noreply.github.com>
2023-09-09 14:58:43 +0530

    Added solution - LeetHub

--- Permutations of a given string - GFG/permutations-of-a-given-string.cpp
+++ Permutations of a given string - GFG/permutations-of-a-given-string.cpp
@@ -3,31 +3,27 @@
 using namespace std;
 
 // } Driver Code Ends
+
 class Solution
 {
     
-    void solve(vector<string> &ans, string &s, string &curr, vector<bool> &visited)
+    void solve(int idx, unordered_set<string> &ans, string &curr, string &s)
     {
-        if(curr.length() == s.length())
-        {
-            ans.push_back(curr);
+        int n = s.length();
+        if(idx == n) {
+            ans.insert(curr);
             return;
         }
-        
-        for(int i = 0; i < s.length(); i++)
-        {
-            if(!visited[i])
-            {
-                if(i > 0 && visited[i - 1] == false && s[i] == s[i - 1])
-                    continue;
-                visited[i] = true;
-                curr.push_back(s[i]);
-                solve(ans, s, curr, visited);
-                curr.pop_back();
-                visited[i] = false;
-            }
+     
+        for(int i = idx; i < n; i++) {
+            if(i > idx && s[i] == s[i - 1])
+                continue;
+            curr.push_back(s[i]);
+            swap(s[i], s[idx]);
+            solve(idx + 1, ans, curr, s);
+            curr.pop_back();
+            swap(s[i], s[idx]);
         }
-        
     }
     
 	public:
@@ -35,16 +31,17 @@ class Solution
 		{
 		    // Code here there
 		    sort(s.begin(), s.end());
-		    vector<string> ans;
+		    unordered_set<string> ans;
 		    string curr = "";
 		    
-		    vector<bool> visited(s.length(), false);
+		    solve(0, ans, curr, s);
 		    
-		    solve(ans, s, curr, visited);
+		    vector<string> result;
+		    for(auto &x : ans) {
+		        result.push_back(x);
+		    }
 		    
-		    sort(ans.begin(), ans.end());
-		    
-		    return ans;
+		    return result;
 		}
 };
 
@@ -60,6 +57,7 @@ int main(){
 	    cin >> S;
 	    Solution ob;
 	    vector<string> ans = ob.find_permutation(S);
+	    sort(ans.begin(),ans.end());
 	    for(auto i: ans)
 	    {
 	    	cout<<i<<" ";
